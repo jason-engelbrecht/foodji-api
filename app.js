94f2383c345601f504a54c02ee54fffb7e8d29b7
Jason Engelbrecht <47205928+jason-engelbrecht@users.noreply.github.com>
2020-07-29 16:43:36 -0700

    Project setup

---
+++ app.js
@@ -0,0 +1,34 @@
+import express from 'express';
+import createError from "http-errors";
+import cookieParser from "cookie-parser";
+import logger from "morgan";
+import cors from "cors";
+import indexRoutes from "./src/routes/index";
+
+const app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(cors());
+
+app.use('/', indexRoutes);
+
+// catch 404 and forward to error handler
+app.use((req, res, next) => {
+  next(createError(404));
+});
+
+// error handler
+app.use((err, req, res, next) => {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.json('Error occurred, please try again');
+});
+
+module.exports = app;
