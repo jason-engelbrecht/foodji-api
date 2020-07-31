7dd6480454a2d184f9f56f22553112ab9ab3127f
Jason Engelbrecht <47205928+jason-engelbrecht@users.noreply.github.com>
2020-07-30 18:14:15 -0700

    Setup basic controllers

--- app.js
+++ app.js
@@ -3,7 +3,10 @@ import createError from "http-errors";
 import cookieParser from "cookie-parser";
 import logger from "morgan";
 import cors from "cors";
+
 import indexController from "./src/controllers/index_controller";
+import userController from "./src/controllers/user_controller";
+import recipeController from "./src/controllers/recipe_controller";
 
 const app = express();
 
@@ -14,6 +17,8 @@ app.use(cookieParser());
 app.use(cors());
 
 app.use('/', indexController);
+app.use('/users', userController);
+app.use('/recipes', recipeController);
 
 //catch 404 and forward to error handler
 app.use((req, res, next) => {
---
+++ src/services/user_service.js
@@ -0,0 +1,9 @@
+import Service from "./service";
+
+export default class UserService extends Service {
+
+    constructor() {
+        super();
+        console.log("I am a user service");
+    }
+}
