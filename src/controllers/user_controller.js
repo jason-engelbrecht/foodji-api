74ec663d3de400c6c0b0327a0060ed0c294f9d03
Jason Engelbrecht <47205928+jason-engelbrecht@users.noreply.github.com>
2020-07-30 18:29:00 -0700

    Start building services

--- src/controllers/user_controller.js
+++ src/controllers/user_controller.js
@@ -1,8 +1,11 @@
 import express from 'express';
+import UserService from "../services/user_service";
 
 const router = express.Router();
+const userService = new UserService();
 
 router.get('/', (req, res) => {
+    userService.registerNewUser('email', 'password');
     res.send('I am the user controller');
 });
 
--- src/services/user_service.js
+++ src/services/user_service.js
@@ -1,9 +1,22 @@
 import Service from "./service";
+import {User} from '../models/user';
 
 export default class UserService extends Service {
 
     constructor() {
         super();
         console.log("I am a user service");
+    }
+
+    registerNewUser(email, password) {
+        const newUser = new User({
+            email: email,
+            password: password
+        });
+
+        newUser.save((err, newUser) => {
+            if (err) return console.error(err);
+            console.log(newUser);
+        });
     }
 }
